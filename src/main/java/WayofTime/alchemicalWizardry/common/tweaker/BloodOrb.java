0350fb87149680c659d9ce842c147e9aaf2a15f1
Raven Szewczyk <git@eigenraven.me>
2023-07-04 22:11:37 +0100

    Only use GTNH CT APIs when GTNH CraftTweaker is present (#46)
    * Update buildscript&dependencies
    
    * Only use GTNH CT APIs when GTNH CraftTweaker is present
    
    * Clean up post-rfg migration
    
    * Add warning to the log

--- src/main/java/WayofTime/alchemicalWizardry/common/tweaker/BloodOrb.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/tweaker/BloodOrb.java
@@ -7,67 +7,76 @@ import static WayofTime.alchemicalWizardry.common.tweaker.MTHelper.toStack;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+
+import WayofTime.alchemicalWizardry.api.items.ShapedBloodOrbRecipe;
 import WayofTime.alchemicalWizardry.api.items.ShapelessBloodOrbRecipe;
 import minetweaker.IUndoableAction;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.IIngredient;
 import minetweaker.api.item.IItemStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.IRecipe;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
-import WayofTime.alchemicalWizardry.api.items.ShapedBloodOrbRecipe;
 
 /**
  * MineTweaker3 Blood Orb Recipe Handler by joshie *
  */
 @ZenClass("mods.bloodmagic.BloodOrb")
-public class BloodOrb 
-{
+public class BloodOrb {
 
-    @ZenMethod
-    public static void addShaped(IItemStack output, IIngredient[][] ingredients) 
-    {
-        MineTweakerAPI.apply(new Add(false, toStack(output), toShapedObjects(ingredients)));
+    public static List<IUndoableAction> lateAdditions = new ArrayList<>();
+    public static List<IUndoableAction> lateRemovals = new ArrayList<>();
+
+    public static void applyAdditionsAndRemovals() {
+        for (IUndoableAction toRemove : lateRemovals) {
+            MineTweakerAPI.apply(toRemove);
+        }
+        for (IUndoableAction toAdd : lateAdditions) {
+            MineTweakerAPI.apply(toAdd);
+        }
+
+        lateAdditions.clear();
     }
 
     @ZenMethod
-    public static void addShapeless(IItemStack output, IIngredient[] ingredients) 
-    {
-        MineTweakerAPI.apply((new Add(true, toStack(output), toObjects(ingredients))));
+    public static void addShaped(IItemStack output, IIngredient[][] ingredients) {
+        lateAdditions.add(new Add(false, toStack(output), toShapedObjects(ingredients)));
+    }
+
+    @ZenMethod
+    public static void addShapeless(IItemStack output, IIngredient[] ingredients) {
+        lateAdditions.add((new Add(true, toStack(output), toObjects(ingredients))));
     }
 
     private static class Add implements IUndoableAction {
+
         private IRecipe iRecipe;
         private final boolean isShapeless;
         private final ItemStack output;
         private final Object[] recipe;
-        
-        public Add(boolean isShapeless, ItemStack output, Object... recipe) 
-        {
+
+        public Add(boolean isShapeless, ItemStack output, Object... recipe) {
             this.isShapeless = isShapeless;
             this.output = output;
             this.recipe = recipe;
         }
-        
+
         @Override
-        public void apply() 
-        {
+        public void apply() {
             if (isShapeless) iRecipe = new ShapelessBloodOrbRecipe(output, recipe);
             else iRecipe = new ShapedBloodOrbRecipe(output, recipe);
             CraftingManager.getInstance().getRecipeList().add(iRecipe);
         }
 
         @Override
-        public boolean canUndo() 
-        {
+        public boolean canUndo() {
             return CraftingManager.getInstance().getRecipeList() != null;
         }
 
         @Override
-        public void undo() 
-        {
+        public void undo() {
             CraftingManager.getInstance().getRecipeList().remove(iRecipe);
         }
 
@@ -77,38 +86,36 @@ public class BloodOrb
         }
 
         @Override
-        public String describeUndo() 
-        {
+        public String describeUndo() {
             return "Removing Blood Orb Recipe for " + output.getDisplayName();
         }
 
         @Override
-        public Object getOverrideKey() 
-        {
+        public Object getOverrideKey() {
             return null;
         }
     }
 
     @ZenMethod
     public static void removeRecipe(IItemStack output) {
-        MineTweakerAPI.apply(new Remove(toStack(output)));
+        lateRemovals.add(new Remove(toStack(output)));
     }
 
     private static class Remove implements IUndoableAction {
+
         private final ItemStack output;
         private IRecipe iRecipe;
-        
-        public Remove(ItemStack output) 
-        {
+
+        public Remove(ItemStack output) {
             this.output = output;
         }
-        
+
         @Override
-        public void apply() 
-        {
-            for (IRecipe r : (List<IRecipe>) CraftingManager.getInstance().getRecipeList()) 
-            {
-                if((r instanceof ShapedBloodOrbRecipe || r instanceof ShapelessBloodOrbRecipe) && r.getRecipeOutput() != null && r.getRecipeOutput().isItemEqual(output)) {
+        public void apply() {
+            for (IRecipe r : (List<IRecipe>) CraftingManager.getInstance().getRecipeList()) {
+                if ((r instanceof ShapedBloodOrbRecipe || r instanceof ShapelessBloodOrbRecipe)
+                        && r.getRecipeOutput() != null
+                        && r.getRecipeOutput().isItemEqual(output)) {
                     iRecipe = r;
                     break;
                 }
@@ -117,14 +124,12 @@ public class BloodOrb
         }
 
         @Override
-        public boolean canUndo() 
-        {
+        public boolean canUndo() {
             return CraftingManager.getInstance().getRecipeList() != null && iRecipe != null;
         }
 
         @Override
-        public void undo() 
-        {
+        public void undo() {
             CraftingManager.getInstance().getRecipeList().add(iRecipe);
         }
 
@@ -134,14 +139,12 @@ public class BloodOrb
         }
 
         @Override
-        public String describeUndo() 
-        {
+        public String describeUndo() {
             return "Restoring Blood Orb Recipe for " + output.getDisplayName();
         }
 
         @Override
-        public Object getOverrideKey() 
-        {
+        public Object getOverrideKey() {
             return null;
         }
     }
