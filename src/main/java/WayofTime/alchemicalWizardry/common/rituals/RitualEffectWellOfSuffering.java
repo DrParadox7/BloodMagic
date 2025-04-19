6dbd70a2cb9bda3ad425afcd20d102dd3822ce8a
koolkrafter5 <creepersrgreen@gmail.com>
2025-04-19 10:28:39 -0400

    Fix rituals not working when doDaylightCycle is set to false. (#72)

--- src/main/java/WayofTime/alchemicalWizardry/common/rituals/RitualEffectWellOfSuffering.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/rituals/RitualEffectWellOfSuffering.java
@@ -38,7 +38,7 @@ public class RitualEffectWellOfSuffering extends RitualEffect {
         int y = ritualStone.getYCoord();
         int z = ritualStone.getZCoord();
 
-        if (world.getWorldTime() % timeDelay != 0) {
+        if (world.getTotalWorldTime() % timeDelay != 0) {
             return;
         }
 
